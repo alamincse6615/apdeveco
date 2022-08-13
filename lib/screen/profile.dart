cc34e512db341e11ad9e4cd6147d7dfbc7c6053b
Prodipbiswas1 <prodip77@gmail.com>
2022-08-13 17:16:53 +0600

    Edit profile

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -6,13 +9,163 @@ class Profile extends StatefulWidget {
   @override
   State<Profile> createState() => _ProfileState();
 }
-
+final nameController = TextEditingController();
+final emailController = TextEditingController();
+File? captureImage;
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Profile Page",style: TextStyle(fontSize: 30),),),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 1,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.green,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.green,
+            ),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  child: Stack(
+                    alignment: AlignmentDirectional.bottomEnd,
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          maxRadius: 100,
+                          child: captureImage == null
+                              ? CircleAvatar(
+                            maxRadius: 100,
+                            backgroundImage: AssetImage("images/download.png"),
+                          )
+                              : CircleAvatar(
+                            maxRadius: 100,
+                            backgroundImage: FileImage(
+                              captureImage!,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        right: 80,
+                        bottom:5,
+                        child: TextButton.icon(
+                            onPressed: () {
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    return Container(
+                                      height: MediaQuery.of(context).size.height/6,
+                                      width: MediaQuery.of(context).size.width,
+                                      child: Column(
+                                        children: [
+                                          FlatButton.icon(
+                                              onPressed: () {
+                                                ImageFile();
+                                              },
+                                              icon: Icon(Icons.browse_gallery_rounded),
+                                              label:
+                                              Text("Gallery")),
+                                          FlatButton.icon(
+                                              onPressed: () {
+                                                imagecamera();
+                                              },
+                                              icon: Icon(Icons.camera),
+                                              label:
+                                              Text("Camera")),
 
+                                        ],
+                                      ),
+
+                                    );
+                                  });
+                              //  Navigator.pop(context);
+
+                            },
+                            icon: Icon(
+                              Icons.camera_alt_outlined,
+                              color: Colors.black,
+                            ),
+                            label: Text("")),
+                      ),
+                    ],
+
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Enter Your Name')
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Enter Your Email'),
+                ),
+              ),
+              ElevatedButton
+                (
+                  onPressed: () {
+                    registration();
+                  },
+                  child: Text("Save")),
+            ],
+          ),
+        ),
+      ),
     );
+
+  }
+  imagecamera() async {
+    var image = await ImagePicker().pickImage(source: ImageSource.camera);
+    if (image != null) {
+      captureImage = File(image.path);
+      setState(() {});
+    }
+  }
+  ImageFile() async {
+    var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      captureImage = File(image.path);
+      setState(() {});
+    }
+  }
+  registration() {
+    String name = nameController.text.toString();
+    String email = emailController.text.toString().trim();
+
+
+    var data = {
+      "Images": captureImage,
+      "name": name,
+      "email": email,
+    };
+
+
   }
 }
+
+
